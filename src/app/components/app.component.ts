50dc093af6c81a69e80c850e962cea810cab3277
dmitryprogrammer <dpischalka@gmail.com>
2025-02-19 15:38:27 +0300

    fix(editor-tree): app component fixes.

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -1,11 +1,14 @@
-import {Component} from "@angular/core";
-import {RouterOutlet} from "@angular/router";
+import {ChangeDetectionStrategy, Component} from "@angular/core";
+import {FoldersTreeComponent} from "../containers/folders-tree/components/folder-tree/folders-tree.component";
+import {EditorComponent} from "../containers/editor/components/editor/editor.component";
 
 @Component({
   selector: "app-root",
-  imports: [RouterOutlet],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [FoldersTreeComponent, EditorComponent],
 })
 export class AppComponent {
   title = "angular-explorer-app";
