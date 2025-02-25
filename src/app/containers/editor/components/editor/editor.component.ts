3a226504e15590f502295c46bc6c5fd8abf40b31
dmitryprogrammer <dpischalka@gmail.com>
2025-02-25 22:37:01 +0300

    feat(editor-tree): added directory name control.

--- src/app/containers/editor/components/editor/editor.component.ts
+++ src/app/containers/editor/components/editor/editor.component.ts
@@ -1,9 +1,14 @@
 import {Component} from "@angular/core";
+import {InputTextModule} from "primeng/inputtext";
+import {FloatLabel} from "primeng/floatlabel";
+import {FormControl, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: "app-editor",
-  imports: [],
+  imports: [ReactiveFormsModule, InputTextModule, FloatLabel],
   templateUrl: "./editor.component.html",
   styleUrl: "./editor.component.scss",
 })
-export class EditorComponent {}
+export class EditorComponent {
+  public directoryName: FormControl<string | null> = new FormControl<string | null>("");
+}
